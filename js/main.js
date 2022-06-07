8d3a4dda1f4d201e29a25ac369b0eeac4b4caa76
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2022-06-07 16:07:10 +0200

    Update Solution

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-let accordion = document.querySelector(".accordion")
+let accordion = document.querySelector(".accordion");
 let questions = document.querySelectorAll(".question");
 let answers = document.querySelectorAll(".answer");
 
 accordion.onclick = (e) => {
-    for (i = 0; i < questions.length; i++) {
-        if (e.target === questions[i]) {
-            questions[i].classList.toggle("active");
-        } else {
-            questions[i].classList.remove("active");
-        }
-        if (e.target.parentElement === answers[i].parentElement) {
-            answers[i].classList.toggle("active");
-        } else {
-            answers[i].classList.remove("active");
-        }
+  for (i = 0; i < questions.length; i++) {
+    if (e.target === questions[i]) {
+      questions[i].classList.toggle("active");
+    } else {
+      questions[i].classList.remove("active");
     }
-}
+    if (e.target.parentElement === answers[i].parentElement) {
+      if (
+        answers[i].style.maxHeight ||
+        answers[i].classList.contains("active")
+      ) {
+        answers[i].style.maxHeight = null;
+        answers[i].classList.remove("active");
+      } else {
+        answers[i].style.maxHeight = answers[i].scrollHeight + "px";
+        answers[i].classList.add("active");
+      }
+      //   answers[i].classList.toggle("active");
+      // } else {
+      //   answers[i].classList.remove("active");
+    } else {
+      answers[i].style.maxHeight = null;
+      answers[i].classList.remove("active");
+    }
+  }
+};
